c06cd4edafab4a0216b744d45f436e01fb7dc2ff
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2023-08-29 23:18:57 +0100

    Fixed carousel ish

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
 const navigationItems = document.querySelectorAll(".nav_link");
 // const Items = document.querySelectorAll(".nav_link");
-window.addEventListener('load', AOS.refresh)
+window.addEventListener("load", AOS.refresh);
 
-navigationItems.forEach((item) => {
+navigationItems.forEach(item => {
   item.addEventListener("click", () => {
     const targetId = item.innerText.toLowerCase().replace(" ", "-");
     const targetElement = document.getElementById(targetId);
     // const body = document.getElementsByTagName("body");
 
     // Remove "active_link" class from all navigation items
-    navigationItems.forEach((navItem) => {
+    navigationItems.forEach(navItem => {
       navItem.classList.remove("active_link");
       navItem.classList.add("inactive_link");
     });
@@ -30,18 +30,18 @@ const sections = document.querySelectorAll(".section");
 const observerOptions = {
   root: null,
   rootMargin: "0px",
-  threshold: 0.5, // Adjust the threshold as needed
+  threshold: 0.5 // Adjust the threshold as needed
 };
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
     const targetId = entry.target.id;
-    const navigationItem = Array.from(navigationItems).find((item) =>
+    const navigationItem = Array.from(navigationItems).find(item =>
       item.textContent.toLowerCase().includes(targetId)
     );
 
     if (entry.isIntersecting) {
-      navigationItems.forEach((navItem) => {
+      navigationItems.forEach(navItem => {
         navItem.classList.add("inactive_link");
         navItem.classList.remove("active_link");
       });
@@ -54,9 +54,38 @@ const observer = new IntersectionObserver((entries) => {
   });
 }, observerOptions);
 
-sections.forEach((section) => {
+sections.forEach(section => {
   console.log(section);
   observer.observe(section);
+});
+
+var swiper = new Swiper(".mySwiper", {
+  // slidesPerView: 5,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    648: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    920: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+    },
+  },
+  // spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 0 //add
+  },
+  speed: 7500,
+  freemode: true
 });
 
 $(document).ready(function () {
@@ -65,8 +94,10 @@ $(document).ready(function () {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 0,
+    // useTransform: false,
+    easing: "swing",
     speed: 1800,
-    cssEase:'linear',
+    cssEase: "linear",
     arrows: false,
     dots: false,
     pauseOnHover: false,
@@ -74,21 +105,21 @@ $(document).ready(function () {
       {
         breakpoint: 920,
         settings: {
-          slidesToShow: 4,
-        },
+          slidesToShow: 4
+        }
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 3,
-        },
+          slidesToShow: 3
+        }
       },
       {
         breakpoint: 520,
         settings: {
-          slidesToShow: 2,
-        },
-      },
-    ],
+          slidesToShow: 2
+        }
+      }
+    ]
   });
 });
