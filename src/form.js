98dea1c933aadb1f618f99777aa928c3662047dc
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2024-07-21 19:51:45 +0100

    New updates

--- src/form.js
+++ src/form.js
@@ -11,7 +11,7 @@ const SubmitContactForm = (e) =>{
     // error_response_indicator.style.display="none";
     // correct_response_indicator.style.display="none";
     axios.defaults.headers.post['Content-Type'] = 'application/json';
-    axios.post('https://formsubmit.co/feyisayoamujoyegbe@gmail.com', {
+    axios.post('https://formsubmit.co/contact@mieladigital.com', {
     _subject: "Contact Form Questions/Inquiries",
     name: author.value,
     Email: email.value,
