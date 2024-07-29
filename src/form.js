6bddd4a5f963869784ebed955aa57ae679c92820
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2024-07-29 15:47:54 +0100

    Fixes requested

--- src/form.js
+++ src/form.js
@@ -3,6 +3,7 @@ const email = document.getElementById('email');
 const subject = document.getElementById('subject');
 const message = document.getElementById('message');
 const contact_us_form = document.getElementById('homeContactForm');
+const selectType = document.getElementById("selectType")
 // const correct_response_indicator = document.getElementById('correct_response');
 // const error_response_indicator = document.getElementById('error_response');
 
@@ -13,10 +14,12 @@ const SubmitContactForm = (e) =>{
     axios.defaults.headers.post['Content-Type'] = 'application/json';
     axios.post('https://formsubmit.co/contact@mieladigital.com', {
     _subject: "Contact Form Questions/Inquiries",
-    name: author.value,
+    Select: selectType.value,
+    Name: author.value,
     Email: email.value,
     Subject: subject.value,
-    Message: message.value
+    Message: message.value,
+    
     })
     .then(response => response.status == 200 ? (console.log("success")): null)
     .catch(error => (console.log(error)));
